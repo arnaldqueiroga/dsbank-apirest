951dbc8f7715e20a25d5a55ae6f5f32535cb52e9
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-12-28 12:15:24 -0300

    Populando o banco H2

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_cliente  (nome, email,  cpf,  data_de_nascimento) VALUES ('Anthony Alexandre', 'Anthony_Alexandre@email.com', '896.160.092-30', TIMESTAMP WITHOUT TIME ZONE '1970-03-16T20:59:19Z' );
+INSERT INTO tb_cliente  (nome, email,  cpf,  data_de_nascimento) VALUES ('Daiane Isabella', 'daiane_isabele@email.com', '582.999.292-29', TIMESTAMP WITHOUT TIME ZONE '1967-01-26T20:59:19Z' );
+INSERT INTO tb_cliente  (nome, email,  cpf,  data_de_nascimento) VALUES ('Danilo Nicolas', 'danilo_nicolas@email.com', '029.840.208-47', TIMESTAMP WITHOUT TIME ZONE '1977-07-07T20:59:19Z' );
+INSERT INTO tb_cliente  (nome, email,  cpf,  data_de_nascimento) VALUES ('Daniel Hugo', 'hugo_daniel@email.com', '515.102.605-44', TIMESTAMP WITHOUT TIME ZONE '2000-12-19T20:59:19Z' );
+INSERT INTO tb_cliente  (nome, email,  cpf,  data_de_nascimento) VALUES ('Nelson Murilo', 'nelson_murilo@email.com', '515.102.605-44', TIMESTAMP WITHOUT TIME ZONE '1977-05-15T20:59:19Z' );
+
+
+
