884d626c648a2e16d7359468f2c75cc7d57f5184
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-12-28 06:58:56 -0300

    Classe Cliente criada

---
+++ backend/src/main/java/com/apirest/dsbank/entities/Cliente.java
@@ -0,0 +1,94 @@
+package com.apirest.dsbank.entities;
+
+import java.time.Instant;
+
+public class Cliente {
+	private Long id;
+	private String nome;
+	private String email;
+	private String cpf;
+	
+	private Instant dataDeNascimento;
+	
+	// Criação do Construtor padrão
+	public Cliente () {
+		
+	}
+	
+	// Construtor com argumento
+	public Cliente(Long id, String nome, String email, String cpf, Instant dataDeNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Instant getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(Instant dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
