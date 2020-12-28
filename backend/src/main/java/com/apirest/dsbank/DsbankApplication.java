f215dabd0d05c5f996b43853997b42956867ac90
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-12-28 06:40:13 -0300

    Projeto criado

---
+++ backend/src/main/java/com/apirest/dsbank/DsbankApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.dsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbankApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/apirest/dsbank/DsbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.dsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
