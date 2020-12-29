5ad2042e529944b9ce35ab8b6f2036c8533d36c1
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-12-29 14:54:39 -0300

    Metodo Put

--- backend/src/main/java/com/apirest/dsbank/resources/exceptions/ResourcesExceptionHandler.java
+++ backend/src/main/java/com/apirest/dsbank/resources/exceptions/ResourcesExceptionHandler.java
@@ -11,15 +11,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.apirest.dsbank.services.exceptions.EntityNotFoundException;
+import com.apirest.dsbank.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice // Anotation que irá permitir que a classe intercepte alguma exceção que aconteça na camada
 //de Resource (controlador Rest), para tratar a exceção
 
 public class ResourcesExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<ErroPadrao> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErroPadrao> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		ErroPadrao err = new ErroPadrao();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/java/com/apirest/dsbank/services/ClienteService.java
+++ backend/src/main/java/com/apirest/dsbank/services/ClienteService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,45 +13,61 @@ import org.springframework.transaction.annotation.Transactional;
 import com.apirest.dsbank.dto.ClienteDTO;
 import com.apirest.dsbank.entities.Cliente;
 import com.apirest.dsbank.repositories.ClienteRepository;
-import com.apirest.dsbank.services.exceptions.EntityNotFoundException;
+import com.apirest.dsbank.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repository;
-	
-	@Transactional (readOnly = true)	
+
+	@Transactional(readOnly = true)
 	public List<ClienteDTO> findAll() {
 		List<Cliente> list = repository.findAll();
 		return list.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
-		
+
 	}
-	
+
 	// Método para busca
 	@Transactional(readOnly = true)
 	public ClienteDTO findById(Long id) {
 		Optional<Cliente> obj = repository.findById(id);
-		Cliente entity = obj.orElseThrow(() -> new EntityNotFoundException("Cliente não encontrado"));
+		Cliente entity = obj.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
 		return new ClienteDTO(entity);
 	}
-	
+
 	// Método Insert (inserir)
 	@Transactional
-	public ClienteDTO insert (ClienteDTO dto) {
+	public ClienteDTO insert(ClienteDTO dto) {
 		Cliente entity = new Cliente();
 		entity.setNome(dto.getNome());
 		entity.setEmail(dto.getEmail());
 		entity.setCpf(dto.getCpf());
 		entity.setdataDeNascimento(dto.getDataDeNascimento());
 		entity = repository.save(entity);
-		return new ClienteDTO(entity);	
-		
-		
+		return new ClienteDTO(entity);
+
 	}
-	
-	
-	
-	
+
+	// Método update (Atualizar)
+	@Transactional
+	public ClienteDTO update(Long id, ClienteDTO dto) {
+		try {
+			Cliente entity = repository.getOne(id);
+			entity.setNome(dto.getNome());
+			entity.setEmail(dto.getEmail());
+			entity.setCpf(dto.getCpf());
+			entity.setdataDeNascimento(dto.getDataDeNascimento());
+			entity = repository.save(entity);
+			return new ClienteDTO(entity);
+			
+		}	
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(" Id não encontrado " +  id );
+			
+		}
+		
+
+	}
 
 }
